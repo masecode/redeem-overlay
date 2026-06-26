56ca792e3adc026c5eac38f4a7e2f066dd92d163
Mason Doti <codermason@outlook.com>
2026-06-26 00:49:53 -0600

    build: Add Cargo.toml files and flake.nix files.

---
+++ flake.nix
@@ -0,0 +1,24 @@
+{
+  description = "Development environment for redeem-overlay";
+
+  inputs.nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+
+  outputs =
+    { self, nixpkgs }:
+    let
+      system = "x86_64-linux";
+      pkgs = nixpkgs.legacyPackages.${system};
+    in
+    {
+      devShells.${system}.default = pkgs.mkShell {
+        packages = with pkgs; [
+          rustc
+          cargo
+          rust-analyzer
+          clippy
+          rustfmt
+        ];
+      };
+    };
+
+}
