04d2e455da529931b10db64f288c6f1eac40c32b
Mason Doti <codermason@outlook.com>
2026-06-26 01:54:07 -0600

    feat!: Add OAuth catch method for redirect

--- src/auth.rs
+++ src/auth.rs
@@ -1,3 +1,7 @@
+use std::io::{BufRead, BufReader, Write};
+use std::net::TcpListener;
+
+/// Builds the authorization URL for the Twitch application.
 pub fn build_authorization(client_id: &str, port: u16, state: &str) -> String {
     // TODO: Format URL string.
     // https://id.twitch.tv/oauth2/authorize?[parameters]
@@ -5,4 +9,47 @@ pub fn build_authorization(client_id: &str, port: u16, state: &str) -> String {
         "https://id.twitch.tv/oauth2/authorize?response_type=code&client_id={}&redirect_uri=http%3A%2F%2Flocalhost%3A{}&scope=channel%3Aread%3Aredemptions&state={}",
         client_id, port, state
     )
+}
+
+pub fn wait_for_code(port: u16, expected_state: &str) -> String {
+    let addr = format!("127.0.0.1:{}", port);
+    let listener = TcpListener::bind(&addr).unwrap();
+    let mut code = "";
+    let mut state = "";
+
+    println!("Listening on {} for redirect..", port);
+
+    for stream in listener.incoming() {
+        let mut stream = stream.unwrap();
+        let mut reader = BufReader::new(&stream);
+        let mut request_line = String::new();
+        reader.read_line(&mut request_line).unwrap();
+
+        let path = request_line.split_whitespace().nth(1).unwrap();
+
+        let parameters = path.split_once("?").unwrap().1.split("&");
+
+        for parameter in parameters {
+            let current_parameter = parameter.split_once("=").unwrap();
+            if current_parameter.0 == "code" {
+                code = current_parameter.1;
+            } else if current_parameter.0.starts_with("state") {
+                state = current_parameter.1;
+            }
+        }
+
+        if state != expected_state {
+            panic!("Returned state was not the expected state.");
+        }
+
+        let body = "You can close this tab now, received.";
+        let response = format!(
+            "HTTP/1.1 200 OK\r\nContent-Length: {} \r\n\r\n{}",
+            body.len(),
+            body
+        );
+        stream.write_all(response.as_bytes()).unwrap();
+        return code.to_string();
+    }
+    unreachable!();
 }
--- src/main.rs
+++ src/main.rs
@@ -4,8 +4,11 @@ mod auth;
  * Main method.
  */
 fn main() {
-    println!(
-        "{}",
-        auth::build_authorization("client_id", 3000, "temp_state_123")
-    )
+    webbrowser::open(&auth::build_authorization(
+        "client_id", // Placeholder. TODO: Get config.toml parser working.
+        3000,
+        "temp_state_123",
+    ));
+    let code = auth::wait_for_code(3000, "temp_state_123");
+    println!("Got code: {}", code);
 }
