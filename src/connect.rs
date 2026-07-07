802bc56e92e0e6d7ce230661915534e020f96f16
Mason Doti <codermason@outlook.com>
2026-07-06 18:56:17 -0600

    Revert "refactor(connect): Refactor common variables into their own constants"
    This reverts commit 38ce6477d1aad7c4099e43eb5c2240512add3e48.

--- src/connect.rs
+++ src/connect.rs
@@ -60,8 +60,7 @@ pub struct ResponseArray {
 }
 
 pub async fn connect(access_token: &str, client_id: &str, user_id: &str) -> anyhow::Result<()> {
-    const TWITCH_WEBSOCKET_URL: &str = "ws://localhost:8080/ws";
-    let (ws_stream, response) = connect_async(TWITCH_WEBSOCKET_URL)
+    let (ws_stream, response) = connect_async("wss://eventsub.wss.twitch.tv/ws")
         .await
         .expect("Failed to connect to websocket.");
 
@@ -101,13 +100,11 @@ pub async fn subscribe_to_channel_points(
     client_id: &str,
     broadcaster_user_id: &str,
 ) -> Result<ResponseBody, reqwest::Error> {
-    const TWITCH_SUBSCRIPTIONS_URL: &str = "http://127.0.0.1:8080/eventsub/subscriptions";
-    const CHANNEL_POINTS_TYPE: &str = "channel.channel_points_custom_reward_redemption.add";
     let client = reqwest::Client::new();
 
     let body = RequestBody {
-        r#type: String::from(CHANNEL_POINTS_TYPE),
-        version: String::from("1"),
+        r#type: String::from("channel.channel_points_custom_reward_redemption.add"),
+        version: 1,
         condition: Condition {
             broadcaster_user_id: broadcaster_user_id.to_string(),
         },
@@ -118,7 +115,7 @@ pub async fn subscribe_to_channel_points(
     };
 
     let post_request = client
-        .post(TWITCH_SUBSCRIPTIONS_URL)
+        .post("https://api.twitch.tv/helix/eventsub/subscriptions")
         .header("Authorization", format!("Bearer {}", access_token))
         .header("Client-Id", client_id)
         .header("Content-Type", "application/json")
