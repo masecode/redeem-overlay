c6c14023a8f71fe80c65ac9eaf1615ab97809923
Mason Doti <codermason@outlook.com>
2026-08-04 05:15:29 -0600

    feat!(main): Add secure state string generation

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,5 @@
 use anyhow::Ok;
+use rand::{RngExt, distr::Alphanumeric};
 use std::sync::Arc;
 use tokio::sync::broadcast;
 
@@ -17,13 +18,14 @@ struct SharedState {
  */
 #[tokio::main]
 async fn main() -> anyhow::Result<()> {
+    let expected_state: &str = &generate_random_string(20);
     let config = configparser::parse_configuration_file()?;
     webbrowser::open(&auth::build_authorization(
         &config.client_id,
         3000,
-        "temp_state_123",
+        expected_state,
     ))?;
-    let code = auth::wait_for_code(3000, "temp_state_123")?;
+    let code = auth::wait_for_code(3000, expected_state)?;
     println!("Got code: {}", code);
 
     let tokens = auth::exchange_code(
@@ -52,7 +54,7 @@ async fn main() -> anyhow::Result<()> {
     let twitch_state = shared_state.clone();
 
     tokio::spawn(async move {
-        httpserver::run_server(config.http_port, shared_state).await;
+        let _ = httpserver::run_server(config.http_port, shared_state).await;
     });
     // This has to be LAST. Do not put anything after connect.
     connect::connect(
@@ -61,6 +63,13 @@ async fn main() -> anyhow::Result<()> {
         &broadcaster_user_id,
         twitch_state,
     )
-    .await;
+    .await?;
     Ok(())
+}
+
+fn generate_random_string(length: usize) -> String {
+    let mut rng = rand::rng();
+    (0..length)
+        .map(|_| rng.sample(Alphanumeric) as char)
+        .collect()
 }
--- src/twitch.rs
+++ src/twitch.rs
@@ -1,3 +1,4 @@
+use anyhow::Context;
 use serde::Deserialize;
 
 #[derive(Deserialize)]
