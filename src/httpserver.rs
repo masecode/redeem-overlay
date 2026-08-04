f23f1ec6b042634460e802fa094f78d00a3b0a8f
Mason Doti <codermason@outlook.com>
2026-08-04 05:12:39 -0600

    refactor(http): Clean up unnecessary mutables and confusing var names

--- src/httpserver.rs
+++ src/httpserver.rs
@@ -3,13 +3,11 @@ use crate::SharedState;
 use axum::extract::State;
 use axum::extract::ws::{Message, WebSocket, WebSocketUpgrade};
 use axum::response::IntoResponse;
-use axum::response::Response;
 use axum::{Router, http::StatusCode, response::Html, routing::any, routing::get};
 use futures_util::{SinkExt, StreamExt};
 use std::fs;
-use std::{fs::File, io::Read};
 
-async fn hello() -> (StatusCode, Html<String>) {
+async fn serve_countdown_html() -> (StatusCode, Html<String>) {
     match fs::read_to_string("html/countdown.html") {
         Ok(html) => (StatusCode::OK, Html::from(html)),
         Err(e) => {
@@ -24,13 +22,9 @@ async fn hello() -> (StatusCode, Html<String>) {
     }
 }
 
-async fn index() -> &'static str {
-    "Hello, World!"
-}
-
 pub async fn run_server(port: u16, shared_state: Arc<SharedState>) -> anyhow::Result<()> {
     let app = Router::new()
-        .route("/", get(hello))
+        .route("/", get(serve_countdown_html))
         .route("/ws", any(ws_handler))
         .with_state(shared_state);
     let listener = tokio::net::TcpListener::bind(format!("127.0.0.1:{}", port)).await?;
@@ -45,9 +39,9 @@ async fn ws_handler(
     ws.on_upgrade(|socket| handle_websocket(socket, shared_state))
 }
 
-pub async fn handle_websocket(mut socket: WebSocket, state: Arc<SharedState>) {
+pub async fn handle_websocket(socket: WebSocket, state: Arc<SharedState>) {
     let mut rx = state.tx.subscribe();
-    let (mut sender, mut receiver) = socket.split();
+    let (mut sender, mut _receiver) = socket.split();
     while let Ok(msg) = rx.recv().await {
         println!("Message: {}", msg);
         sender.send(Message::Text("start".into())).await.ok();
